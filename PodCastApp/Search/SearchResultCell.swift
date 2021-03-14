4ac4e85ed3ac45ee3f272ec2de047d5adea8837d
224Apps <224apps@gmail.com>
2021-03-14 14:54:05 -0400

    Styling Custom Cells

--- PodCastApp/Search/SearchResultCell.swift
+++ PodCastApp/Search/SearchResultCell.swift
@@ -13,4 +13,25 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var podcastTitleLabel: UILabel!
     @IBOutlet weak var podcstAuthorLabel: UILabel!
     
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundView = UIView()
+        backgroundView?.backgroundColor = Theme.Colors.gray4
+        backgroundColor = Theme.Colors.gray4
+        
+        selectedBackgroundView = UIView()
+        selectedBackgroundView?.backgroundColor = Theme.Colors.gray3
+        
+        podcastTitleLabel.textColor = Theme.Colors.gray0
+        podcstAuthorLabel.textColor = Theme.Colors.gray1
+    }
+
+    func configure(with searchResult: SearchResultViewModel){
+        podcastTitleLabel.text = searchResult.title
+        podcstAuthorLabel.text = searchResult.author
+    }
+    
 }
---
+++ PodCastApp/Utility/SearchResultViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResultViewModel.swift
+//  PodCastApp
+//
+//  Created by Abdoulaye Diallo on 3/13/21.
+//
+
+import Foundation
+
+
+struct SearchResultViewModel {
+    let title: String
+    let author: String
+    let imageUrl: URL?
+}
